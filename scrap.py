6b2662a55a9b559e54eeca3e0401cd09e49bf762
Ra <rami@jwcorp.fr>
2023-12-26 05:16:06 +0100

    add script + result of script

---
+++ scrap.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_nextjs_docs(base_url, file_name):
+    with open(file_name, 'w') as file:
+        response = requests.get(base_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            links = soup.find_all('a', href=True)
+            for link in links:
+                href = link['href']
+                if href.startswith('/docs'):
+                    full_url = f"{base_url.rstrip('/')}{href}"
+                    print(full_url, file=file)
+        else:
+            print('Failed to retrieve the documentation')
+
+base_url = 'https://nextjs.org/docs'
+file_name = 'nextjs_doc_links.txt'
+scrape_nextjs_docs(base_url, file_name)
